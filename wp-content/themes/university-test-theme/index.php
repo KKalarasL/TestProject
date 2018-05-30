0170766e09fecceb51945eafc08b62bcca1d681a
Konstantinos Kalaras <konstantinos.kalaras@upstreamsystems.com>
2018-05-30 15:38:21 +0300

    Create new theme - PHP Example

---
+++ wp-content/themes/university-test-theme/index.php
@@ -0,0 +1,12 @@
+<?php
+	
+	$names = array('Brad', 'John', 'Jane', 'Meowsalot', 'Barksalot');
+
+	$count = 0;
+
+	while($count < count($names)) {
+		echo "<li>Hi, my name is $names[$count] </li>";
+		$count++;
+	}
+
+?>
